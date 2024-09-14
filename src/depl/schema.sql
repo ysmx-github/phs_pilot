014a602e151275f5a3612013bb46277f3a1e9149
ysmx-github <ysmx-github>
2024-09-14 03:49:36 +0000

    #5

--- src/depl/schema.sql
+++ src/depl/schema.sql
@@ -1,23 +1,23 @@
-create catalog if not exists databricks_ssa;
-create schema if not exists databricks_ssa.clinical_raw;
-create schema if not exists databricks_ssa.clinical_bronze;
-create schema if not exists databricks_ssa.clinical_silver;
+create catalog if not exists clinical;
+create schema if not exists clinical.clinical_raw;
+create schema if not exists clinical.clinical_bronze;
+create schema if not exists clinical.clinical_silver;
 
-create volume if not exists databricks_ssa.clinical_raw.clinical_data_volume;
+create volume if not exists clinical.clinical_raw.clinical_data_volume;
 
--- grant all privileges on catalog databricks_ssa to edm_adm_grp;
--- grant all privileges on schema databricks_ssa.clinical_raw to edm_adm_grp;
--- grant all privileges on schema databricks_ssa.clinical_bronze  to edm_adm_grp;
--- grant all privileges on schema databricks_ssa.clinical_silver to edm_adm_grp;
+-- grant all privileges on catalog clinical to edm_adm_grp;
+-- grant all privileges on schema clinical.clinical_raw to edm_adm_grp;
+-- grant all privileges on schema clinical.clinical_bronze  to edm_adm_grp;
+-- grant all privileges on schema clinical.clinical_silver to edm_adm_grp;
+
+-- grant usage on catalog clinical to edm_etl_grp;
+-- grant all privileges on schema clinical.clinical_raw to edm_etl_grp;
+-- grant all privileges on schema clinical.clinical_bronze  to edm_etl_grp;
+-- grant all privileges on schema clinical.clinical_silver to edm_etl_grp;
 
 -- grant usage on catalog databricks_ssa to edm_etl_grp;
--- grant all privileges on schema databricks_ssa.clinical_raw to edm_etl_grp;
--- grant all privileges on schema databricks_ssa.clinical_bronze  to edm_etl_grp;
--- grant all privileges on schema databricks_ssa.clinical_silver to edm_etl_grp;
+-- grant usage, modify, read volume, refresh, select, write volume on schema clinical.clinical_raw to edm_rw_grp;
+-- grant usage, modify, read volume, refresh, select, write volume on schema clinical.clinical_bronze to edm_rw_grp;
+-- grant usage, modify, read volume, refresh, select, write volume on schema clinical.clinical_silver to edm_rw_grp;
 
--- grant usage on catalog databricks_ssa to edm_etl_grp;
--- grant usage, modify, read volume, refresh, select, write volume on schema databricks_ssa.clinical_raw to edm_rw_grp;
--- grant usage, modify, read volume, refresh, select, write volume on schema databricks_ssa.clinical_bronze to edm_rw_grp;
--- grant usage, modify, read volume, refresh, select, write volume on schema databricks_ssa.clinical_silver to edm_rw_grp;
-
--- drop catalog  databricks_ssa cascade;  
+-- drop catalog clinical cascade;  
